74bfb184e9ba4f54c67ca98efc3477dcb90f3bb2
Michael Olatunji <molatunji371@gmail.com>
2026-07-09 18:19:55 +0100

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
     ? `${import.meta.env.VITE_API_URL}/api`
-    : "http://localhost:8080/api",
+    : "https://np-backend-4ee5.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
